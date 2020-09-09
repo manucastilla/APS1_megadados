601f18c49ad3b7b093abf2024697cd22125a3a30
duda <mariaeduardabicalho@gmail.com>
2020-09-09 17:16:20 -0300

    UUID como pkey

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 # uvicorn main:app --reload
+from typing import Optional
 
 from fastapi import FastAPI, Path
 from pydantic import BaseModel
-from typing import Optional
+import uuid
+from uuid import UUID
 
-app = FastAPI()
 
+app = FastAPI(title='Lista de Tarefas',)
+
+## uuid4 funcao para gerar automatico
 
 class Tasks(BaseModel):
+    id: UUID = uuid.uuid4()
     title: str
     description: Optional[str] = None
     done: bool
@@ -18,17 +23,27 @@ tasks_dict ={}
 def save_task(task:Tasks):
     task_saved = Tasks(**task.dict())
     return task_saved
+   
 
 
-@app.post("/tasks/", response_model=Tasks)
+@app.post("/tasks/", response_model=Tasks, tags=[" Cria tarefas"])
 async def create_task(task: Tasks):
     task_saved = save_task(task)
-    tasks_dict[task.title]= task_saved
+    tasks_dict[task.id]= task_saved
+    print("KEYYYY:::::")
+    print( task.id)
     return task_saved
+        
+
+@app.get("/tasks/{uuid}", response_model=Tasks, response_model_exclude_unset=True, tags=[" Mostra tarefas"])
+async def read_task(uuid :str):
+    return tasks_dict[uuid]
+
+@app.get("/tasks",tags=[" Mostra tarefas"])
+async def read_tasks():
+    return tasks_dict
 
 
-@app.get("/tasks/{task_id}", response_model=Tasks, response_model_exclude_unset=True)
-async def read_task(task_id :str):
-    return tasks_dict[task_id]
+
 
 

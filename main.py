97a5e990c5aac70d716c7c6388f1efdfed97d1a9
duda <mariaeduardabicalho@gmail.com>
2020-09-11 15:41:00 -0300

    finalizada

--- main.py
+++ main.py
@@ -17,16 +17,14 @@ app = FastAPI(title='Lista de Tarefas',)
 class Tasks(BaseModel):
     title: str
     description: Optional[str] = None
-    done: bool
+    done: bool = False
 
 class Tasks_Status(str, Enum):
-    concluida = "concluida"
-    naoconcluida = "naoconcluida"
-    todas="todas"
-
+    finished = "finished"
+    notfinished = "notfinished"
+    allt="allt"
 
 tasks_dict = {}
-
 
 def save_task(task: Tasks):
     task_saved = Tasks(**task.dict())
@@ -43,65 +41,55 @@ async def create_task(task: Tasks):
     return task_saved
 
 
-# @app.get("/listtask/{uuid}", response_model=Tasks, response_model_exclude_unset=True, tags=[" Mostra tarefa especifica"])
-# async def read_task(tid: UUID):
-#     ''' Esse método  mostra uma tarefa quando um id é especificado. '''
-#     return tasks_dict[tid]
-
-
-@app.get("/listtasks/{tasks_status}",tags=[" Mostra tarefas"])
+@app.get("/listtasks/{tasks_status}",response_model=dict,tags=[" Mostra tarefas"])
 async def read_tasks(tasks_status: Tasks_Status):
-    ''' Esse método lista todas as tarefas, podendo escolher a listagem entre todas as tarefas as tarefas concluidas e as tarefas não concluidas'''
+    ''' Esse método lista todas as tarefas, podendo escolher a listagem entre todas as tarefas(parametro allt no path),  as tarefas concluidas (parametro finished no path) e as tarefas não concluidas (parametro notfinished no path)'''
+
     listagem={}
     print(tasks_status)
-    if tasks_status == Tasks_Status.concluida:
+    if tasks_status == Tasks_Status.finished:
         for t in tasks_dict:
             if tasks_dict[t].done == True:
                 listagem[t]=tasks_dict[t]
         return listagem
 
-    elif tasks_status == Tasks_Status.naoconcluida:
+    elif tasks_status == Tasks_Status.notfinished:
         for t in tasks_dict:
             print(tasks_dict[t])
             if tasks_dict[t].done == False:
                 listagem[t]=tasks_dict[t]
         return listagem
 
-    elif tasks_status == Tasks_Status.todas:
+    elif tasks_status == Tasks_Status.allt:
         return tasks_dict
 
-
-
-
-@app.patch("/changetaskdescription/{uuid}",tags=[" Altera descrição da tarefa"])
-async def update_task_description(tid: UUID,newvalue: str):
+@app.patch("/changetaskdescription/{uuid}",response_model=Tasks, response_model_exclude_unset=True,tags=[" Altera descrição da tarefa"])
+async def update_task_description(tid: UUID, newvalue: str ):
     ''' Esse método altera a descrição de uma tarefa. '''
-    # stored_item_data = tasks_dict[tid]
-    # stored_item_model = Tasks(**stored_item_data)
-    # update_data = task.dict(exclude_unset=True)
-    # updated_item = tasks_dict[tid].copy(update=update_data)
-    # tasks_dict[tid] = jsonable_encoder(updated_item)
+
     tasks_dict[tid].description =newvalue
     return tasks_dict[tid]
 
-@app.patch("/changetaskdone/{uuid}",tags=[" Altera status da tarefa"])
+@app.patch("/changetaskdone/{uuid}",response_model=dict,tags=[" Altera status da tarefa"])
 async def update_task_done(tid: UUID, newvalue: bool):
     ''' Esse método altera o status de uma tarefa, se está concluida ou não. '''
     tasks_dict[tid].done =newvalue
     return tasks_dict[tid]
 
+
 @app.put("/updatetasks/{uuid}",response_model=Tasks, response_model_exclude_unset=True, tags=[" Altera tarefa inteira"])
 async def update_task(tid: UUID, task: Tasks):
     ''' Esse método altera uma tarefa pro completo. '''
-    # update_item_encoded = tasks_dict[task.id]  # usa json ou não
+
     update_item_encoded = jsonable_encoder(task)
     tasks_dict[tid] = update_item_encoded
     return update_item_encoded
 
 
-@app.delete("/deltasks/{uuid}", tags=[" Apaga tarefa "])
+@app.delete("/deltasks/{uuid}",response_model=dict, tags=[" Apaga tarefa "])
 async def delete_task(tid: UUID):
     ''' Esse método apaga uma tarefa do nosso banco de dados. '''
+
     tasks_dict.pop(tid,None)
     return tasks_dict
 

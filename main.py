fa42e18cbbc71504e4476f828fd39cb0f36e203e
duda <mariaeduardabicalho@gmail.com>
2020-09-10 22:47:38 -0300

    pronto faltando descricoes

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from pydantic import BaseModel
 import uuid
 from fastapi.encoders import jsonable_encoder
 from uuid import UUID
+from enum import Enum
 
 
 app = FastAPI(title='Lista de Tarefas',)
@@ -13,11 +14,17 @@ app = FastAPI(title='Lista de Tarefas',)
 # uuid4 funcao para gerar automatico
 
 
+
 class Tasks(BaseModel):
     id: UUID = None
     title: str
     description: Optional[str] = None
     done: bool
+
+class Tasks_Status(str, Enum):
+    concluida = "concluida"
+    naoconcluida = "naoconcluida"
+    todas="todas"
 
 
 tasks_dict = {}
@@ -30,23 +37,43 @@ def save_task(task: Tasks):
 
 @app.post("/tasks/", response_model=Tasks, tags=[" Cria tarefas"])
 async def create_task(task: Tasks):
+    ''' Esse método cria uma nova tarefa. Não são necessários parâmetros para chamar esse método.'''
+
     task.id = uuid.uuid4()
     task_saved = save_task(task)
     tasks_dict[task.id] = task_saved
     return task_saved
 
 
-@app.get("/tasks/{uuid}", response_model=Tasks, response_model_exclude_unset=True, tags=[" Mostra tarefas"])
+@app.get("/listTask/{uuid}", response_model=Tasks, response_model_exclude_unset=True, tags=[" Mostra tarefa especifica"])
 async def read_task(uuid: UUID):
     return tasks_dict[uuid]
 
 
-@app.get("/tasks",tags=[" Mostra tarefas"])
-async def read_tasks():
-    return tasks_dict
+@app.get("/listTasks/{tasks_status}",response_model=dict, response_model_exclude_unset=True,tags=[" Mostra tarefas"])
+async def read_tasks(tasks_status: Tasks_Status):
+    ''' Lista todas as tarefas '''
+    listagem={}
+    print(tasks_status)
+    if tasks_status == Tasks_Status.concluida:
+        for t in tasks_dict:
+            if tasks_dict[t].done == True:
+                listagem[tasks_dict[t].id]=tasks_dict[t]
+        return listagem
+
+    elif tasks_status == Tasks_Status.naoconcluida:
+        for t in tasks_dict:
+            print(tasks_dict[t])
+            if tasks_dict[t].done == False:
+                listagem[tasks_dict[t].id]=tasks_dict[t]
+        return listagem
+
+    elif tasks_status == Tasks_Status.todas:
+        return tasks_dict
 
 
-@app.put("/tasks/{uuid}")
+
+@app.put("/updateTasks/{uuid}",response_model=Tasks, response_model_exclude_unset=True, tags=[" Altera tarefa "])
 async def update_task(uuid: UUID, task: Tasks):
     # update_item_encoded = tasks_dict[task.id]  # usa json ou não
     update_item_encoded = jsonable_encoder(task)
@@ -54,7 +81,7 @@ async def update_task(uuid: UUID, task: Tasks):
     return update_item_encoded
 
 
-@app.patch("/tasks/{uuid}")
+@app.patch("/changeTasks/{uuid}",tags=[" Altera caracteritica da tarefa "])
 async def update_partial_task(uuid: UUID, task: Tasks):
     stored_item_data = tasks_dict[uuid]
     stored_item_model = Tasks(**stored_item_data)
@@ -64,7 +91,11 @@ async def update_partial_task(uuid: UUID, task: Tasks):
     return updated_item
 
 
-@app.delete("/tasks/{uuid}")
+
+
+
+
+@app.delete("/delTasks/{uuid}",response_model=Tasks, response_model_exclude_unset=True, tags=[" Apaga tarefa "])
 async def delete_task(uuid: UUID, task: Tasks):
     print(uuid)
     tasks_dict.pop(uuid,None)

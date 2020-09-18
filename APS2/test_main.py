facba1da50c19039c289d50037a5aa1d08a0d0f9
manucastilla <manuela.castilla@gmail.com>
2020-09-18 13:59:17 -0300

    comecando fazer o teste

--- APS2/test_main.py
+++ APS2/test_main.py
@@ -1,17 +1,27 @@
 from fastapi.testclient import TestClient
 from main import app
-
+import uuid
+#import main
 
 client = TestClient(app)
 
-class DBSession:
-    tasks = {}
-    def __init__(self):
-        self.tasks = DBSession.tasks
-    def get_db():
-        return DBSession()
+
+
+# ============ GET ============ #
 
 def test_read_main_returns_not_found():
     response = client.get('/')
     assert response.status_code == 404
     assert response.json() == {'detail': 'Not Found'}
+
+def test_read_id_not_found():
+    uuid_ = uuid.uuid4()
+    response = client.get(f"/task/{uuid_}")
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Task not found"}
+
+def test_read_list():
+    response = client.get("/task")
+    assert response.status_code == 200
+    
+# ============ POST ============ #

3b1d36933bf66069b09ff834cab5a43b641f97fc
manucastilla <manuela.castilla@gmail.com>
2020-11-03 10:56:37 -0300

    atendimento resolvido

--- APS3/tasklist/tasklist/main.py
+++ APS3/tasklist/tasklist/main.py
@@ -1,7 +1,7 @@
 # pylint: disable=missing-module-docstring
 from fastapi import FastAPI
 
-from .routers import task
+from .routers import task, user
 
 tags_metadata = [
     {
@@ -24,4 +24,4 @@ app = FastAPI(
 )
 
 app.include_router(task.router, prefix='/task', tags=['task'])
-app.include_router(task.router, prefix='/user', tags=['user'])
+app.include_router(user.router, prefix='/user', tags=['user'])
